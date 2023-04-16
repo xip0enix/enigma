0ea46c61f1562191eabdead92dea41bf65c2a3be
xip0enix <108542693+xip0enix@users.noreply.github.com>
2023-04-16 15:47:46 +0200

    weather rewrite
    + changed api to open mateo api
    + optimized

--- js/weather.js
+++ js/weather.js
@@ -3,68 +3,107 @@
 //  \ \/\/ / -_) _` |  _| ' \/ -_) '_|
 //  \_/\_/\___\__,_|\__|_||_\___|_|  
 */
-let width = screen.width;
-const iconElement = document.querySelector('.weatherIcon');
-const tempElement = document.querySelector('.weatherValue p');
-const descElement = document.querySelector('.weatherDescription p');
 const weather = {};
 weather.temperature = {
 	unit: 'celsius',
 };
 
-let tempUnit = 'C';
+const weatherIcons = {
+	0: 0,
+	1: 1,
+	2: 2,
+	3: 3,
+	45: 45,
+	51: 51,
+	53: 51,
+	55: 51,
+	56: 51,
+	57: 51,
+	61: 51,
+	63: 51,
+	65: 51,
+	66: 51,
+	67: 51,
+	71: 71,
+	73: 71,
+	75: 71,
+	77: 77,
+	80: 51,
+	81: 51,
+	82: 51,
+	85: 85,
+	86: 85,
+	95: 95,
+	96: 95,
+	99: 95,
+};
 
-const KELVIN = 273.15;
-const apiKey = process.env.OPENWEATHERMAP_API_KEY;
+const weatherDescriptions = {
+	0: 'Clear',
+	1: 'Mostly clear',
+	2: 'Partly cloudy',
+	3: 'Mostly cloudy',
+	45: 'Fog',
+	51: 'Drizzle',
+	53: 'Light rain',
+	55: 'Rain',
+	56: 'Rain',
+	57: 'Heavy rain',
+	61: 'Light snow',
+	63: 'Snow',
+	65: 'Heavy snow',
+	66: 'Snow shower',
+	67: 'Heavy snow shower',
+	71: 'Thunderstorm',
+	73: 'Thunderstorm',
+	75: 'Thunderstorm',
+	77: 'Thunderstorm',
+	80: 'Light rain shower',
+	81: 'Rain shower',
+	82: 'Heavy rain shower',
+	85: 'Light snow shower',
+	86: 'Snow shower',
+	95: 'Thunderstorm',
+	96: 'Thunderstorm',
+	99: 'Thunderstorm',
+};
 
-if (width > 1101) {
-	setPosition();
+const iconElement = document.querySelector('.weatherIcon');
+const tempElement = document.querySelector('.weatherValue p');
+const descElement = document.querySelector('.weatherDescription p');
+const width = screen.width > 1101;
 
-function setPosition(position) {
+if (width) {
 	navigator.geolocation.getCurrentPosition(
 		pos => {
-			getWeather(pos.coords.latitude.toFixed(3), pos.coords.longitude.toFixed(3));
+		  let api = `https://api.open-meteo.com/v1/meteofrance?latitude=${pos.coords.latitude.toFixed(3)}&longitude=${pos.coords.longitude.toFixed(3)}&current_weather=true`;
+		  fetch(api)
+			.then(response => response.json())
+			.then(data => {
+			  weather.temperature.value = data.current_weather.temperature;
+			  weather.iconId = data.current_weather.weathercode;
+			  weather.is_day = data.current_weather.is_day;
+			  displayWeather();
+			})
+			.catch(err => {
+			  console.error(err);
+			  getWeather(49, 11.5);
+			});
 		},
 		err => {
-			console.error(err);
-			getWeather(49, 11.5);
+		  console.error(err);
+		  getWeather(49, 11.5);
 		}
-	);
-}
-
-function convertTemperature(value, fromUnit, toUnit) {
-	if (fromUnit === toUnit) {
-	  return value;
-	}
-	if (fromUnit === 'C' && toUnit === 'F') {
-	  return (value * 9) / 5 + 32;
-	}
-	if (fromUnit === 'F' && toUnit === 'C') {
-	  return (value - 32) * 5 / 9;
-	}
-	return value;
-  }
-
-function getWeather(latitude, longitude) {
-	let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&lang=de&appid=${apiKey}`;
-	fetch(api)
-		.then(function(response) {
-			let data = response.json();
-			return data;
-		})
-		.then(function(data) {
-			weather.temperature.value = convertTemperature(data.main.temp - KELVIN, 'C', tempUnit);
-			weather.description = data.weather[0].description;
-			weather.iconId = data.weather[0].icon;
-		})
-		.then(function() {
-			displayWeather();
-		});
+	  );
 }
 
 function displayWeather() {
-	iconElement.innerHTML = `<img src="assets/icons/Nord/${weather.iconId}.png"/>`;
-	tempElement.innerHTML = `${convertTemperature(weather.temperature.value, 'C', tempUnit).toFixed(0)}°<span class="darkfg">${tempUnit}</span>`;
-	descElement.textContent = weather.description;
-}
+	if (!weather.is_day == 1) {
+		iconElement.innerHTML = `<img src="assets/icons/Nord/${weatherIcons[weather.iconId]}n.png"/>`;
+	}
+	else {
+		iconElement.innerHTML = `<img src="assets/icons/Nord/${weatherIcons[weather.iconId]}.png"/>`;
+	}
+	tempElement.innerHTML = weather.temperature.value + ' °C';
+	descElement.textContent = weatherDescriptions[weather.iconId];
 }
