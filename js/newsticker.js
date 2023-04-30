4bc84ea7271ef75b2f1deaee20db74f90dff2670
xip0enix <108542693+xip0enix@users.noreply.github.com>
2023-04-30 12:57:38 +0200

    update

--- js/newsticker.js
+++ js/newsticker.js
@@ -1,35 +1,75 @@
-function getNews() {
-  fetch('https://www.tagesschau.de/api2u/homepage')
-  .then(response => response.json())
-  .then(data => {
-    const newsArray = data.news;
-    const newsList = document.getElementById('newsList');
+// Selektiere das Element, in dem die News-Liste gerendert wird
+const newsList = document.getElementById('newsList');
 
-    // Entferne alle vorherigen Elemente der Liste
-    while (newsList.firstChild) {
-      newsList.removeChild(newsList.firstChild);
-    }
+// Eine Funktion zum Entfernen aller Child-Nodes eines Parent-Elements
+function removeChildNodes(parentNode) {
+  while (parentNode.firstChild) {
+    parentNode.removeChild(parentNode.firstChild);
+  }
+}
 
-    for(let i = 0; i < newsArray.length-1; i++) {
-      const newsItem = document.createElement('li');
-      const newsLink = document.createElement('a');
-      newsLink.href = newsArray[i].shareURL;
-      newsLink.textContent = newsArray[i].title;
-      newsLink.id = newsArray[i].crop.headerText;
-      newsLink.addEventListener('mouseenter', function() {
-        newsLink.textContent = "#" + newsLink.id;
-      });
-      newsLink.addEventListener('mouseleave', function() {
-        newsLink.textContent = newsArray[i].title;
-      });
-      newsItem.appendChild(newsLink);
-      newsList.appendChild(newsItem);
-    }
-  })
-  .catch(error => {
-    console.error('Fehler beim Abrufen der Daten:', error);
+// Eine Funktion zum Erstellen eines einzelnen News-Items
+function createNewsItem(news) {
+  // Erstelle die notwendigen DOM-Elemente
+  const newsItem = document.createElement('li');
+  const newsLink = document.createElement('a');
+  const newsLinkText = document.createElement('span');
+
+  // Setze die Attribute und Inhalte der DOM-Elemente
+  newsLink.href = news.shareURL;
+  newsLinkText.textContent = news.topline;
+  newsLink.appendChild(document.createTextNode(news.title + ' ['));
+  newsLink.appendChild(newsLinkText);
+  newsLink.appendChild(document.createTextNode(']'));
+  newsLink.id = news.crop.headerText;
+
+  // Füge Event-Listener hinzu, um die Maus-Interaktion zu verarbeiten
+  newsLink.addEventListener('mouseenter', function() {
+    newsLinkText.textContent = newsLink.id;
+  });
+
+  newsLink.addEventListener('mouseleave', function() {
+    newsLinkText.textContent = news.topline;
+  });
+
+  // Füge CSS-Klassen hinzu, um das Styling zu steuern
+  newsLink.classList.add('news-link');
+
+  // Füge das News-Item zum DOM hinzu und gib es zurück
+  newsItem.appendChild(newsLink);
+  return newsItem;
+}
+
+// Eine Funktion zum Rendern aller News-Items
+function renderNews(newsArray) {
+  // Entferne alle vorherigen News-Items aus der Liste
+  removeChildNodes(newsList);
+
+  // Erstelle ein News-Item für jedes Element im Array (bis auf das letzte)
+  newsArray.slice(0, -1).forEach(function(news) {
+    const newsItem = createNewsItem(news);
+    newsList.appendChild(newsItem);
   });
 }
 
-getNews();
-setInterval(getNews, 300000);
+// Eine Funktion zum Abrufen der News-Daten und anschließenden Rendern der News-Items
+function fetchNews() {
+  // Führe einen fetch-Aufruf auf die API-URL durch
+  fetch('https://www.tagesschau.de/api2u/homepage')
+    .then(response => response.json())
+    .then(data => {
+      // Extrahiere das News-Array aus der API-Antwort
+      const newsArray = data.news;
+      // Rendere die News-Items
+      renderNews(newsArray);
+    })
+    .catch(error => {
+      console.error('Fehler beim Abrufen der Daten:', error);
+    });
+}
+
+// Führe die fetchNews-Funktion einmal aus, um die News-Items beim Laden der Seite zu rendern
+fetchNews();
+
+// Führe die fetchNews-Funktion alle 5 Minuten erneut aus, um die News-Items regelmäßig zu aktualisieren
+setInterval(fetchNews, 300000);
